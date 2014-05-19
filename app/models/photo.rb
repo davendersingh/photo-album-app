4a7e8151ebefaa02c58af7288c40fb6ee75d81b3
Davender Singh <dsingh11@gmail.com>
2014-05-19 15:25:52 +0530

    photo model modified

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,9 @@
 class Photo < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, 
-	:url => "/gallery_images/:user_id/:style/:filename.:extension",
-    :path => ":rails_root/public/gallery_images/:user_id/:style/:filename.:extension", 
+	:url => '/gallery_images/:user_id/:style/:filename.:extension',
+	:use_timestamp => false,
+    :path => ':rails_root/public:url', 
 	:styles => { :large => "700x700>", :medium => "400x400>",:small => "200x200", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
